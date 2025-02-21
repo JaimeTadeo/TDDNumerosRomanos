a1184c899261eca07d803f2e8e6d7ebc4c7d9c06
JaimeTadeo <deugartebutron@gmail.com>
2025-02-21 10:48:18 -0400

    feat:test y funcion de 1 y 2

--- src/NumerosRomanos.js
+++ src/NumerosRomanos.js
@@ -1,6 +1,8 @@
 export default function numeros(num) {
     if (num === 1) {
         return "I";
+    } else if (num === 2) {
+        return "II";
     }
     return "";
 }
--- src/NumerosRomanos.test.js
+++ src/NumerosRomanos.test.js
@@ -4,4 +4,8 @@ describe("Numeros Romanos", () => {
     it("debería convertir 1 a I", () => {
         expect(numeros(1)).toEqual("I");
     });
+
+    it("debería convertir 2 a II", () => {
+        expect(numeros(2)).toEqual("II");
+    });
 });
